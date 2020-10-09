e8b144431858f6de0024fb536dd805eed1a1559f
pratik1308 <pratikj316@gmail.com>
2020-10-09 15:23:02 +0530

    fc

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { CommonService } from './common.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mycurd';
+  allUser: Object;
+  isEdit=false;
+  userObj={
+    firstname:'',
+    lastname:'',
+    mobile:'',
+    email:'',
+    password:'',
+    dob:'',
+    state:'',
+    city:'',
+    id:''
+
+  }
+
+  constructor(private commonService:CommonService){}
+  ngOnInit(){
+    this.getLatestUser();
+  }
+  addUser(formObj){
+    console.log(formObj)
+    this.commonService.createUser(formObj).subscribe((response)=>{
+      this.getLatestUser();
+    })
+  }
+  getLatestUser(){
+    this.commonService.getAllUser().subscribe((response)=>{
+      this.allUser = response
+    })
+  }
+  editUser(user){
+    this.isEdit = true;
+    this.userObj = user
+  }
+
+  deleteUser(user){
+    this.commonService.deleteUser(user).subscribe(()=>{
+      this.getLatestUser();
+    })
+
+  }
+
+  updateUser(){
+    this.isEdit = !this.isEdit;
+    this.commonService.updateUser(this.userObj).subscribe(()=>{
+      this.getLatestUser();
+    })
+  }
+}
